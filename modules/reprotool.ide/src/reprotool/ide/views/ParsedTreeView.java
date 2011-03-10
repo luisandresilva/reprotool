6219bb2c88cb942136ef979584bcb1e174cde94a
rudotx <rudotx@gmail.com>
2011-03-10 21:44:14 +0000

    ParsedTreeLayoutAlgorithm removed.

--- modules/reprotool.ide/src/reprotool/ide/views/ParsedTreeView.java
+++ modules/reprotool.ide/src/reprotool/ide/views/ParsedTreeView.java
@@ -1,17 +1,17 @@
 package reprotool.ide.views;
 
+import java.util.Comparator;
+
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.part.ViewPart;
 import org.eclipse.zest.core.viewers.GraphViewer;
 import org.eclipse.zest.layouts.LayoutStyles;
-
-
+import org.eclipse.zest.layouts.algorithms.TreeLayoutAlgorithm;
 import reprotool.ide.parsetree.NodeContentProvider;
 import reprotool.ide.parsetree.NodeLabelProvider;
 import reprotool.ide.parsetree.NodeModelContentProvider;
-import reprotool.ide.parsetree.ParsedTreeLayoutAlgorithm;
 
 public class ParsedTreeView extends ViewPart {
 
@@ -31,8 +31,18 @@ public class ParsedTreeView extends ViewPart {
 		NodeModelContentProvider model = new NodeModelContentProvider();
 		viewer.setInput(model.getNodes());
 
-		ParsedTreeLayoutAlgorithm tla = 
-			new ParsedTreeLayoutAlgorithm(LayoutStyles.NO_LAYOUT_NODE_RESIZING);
+		TreeLayoutAlgorithm tla = 
+			new TreeLayoutAlgorithm(LayoutStyles.NO_LAYOUT_NODE_RESIZING);
+		
+		tla.setComparator(new Comparator() {
+			
+			/* We just keep the original order */
+			@Override
+			public int compare (Object node1, Object node2) {
+				return 0;
+			}
+			
+		});
 		
 		viewer.setLayoutAlgorithm(tla, true);
 		viewer.applyLayout();
