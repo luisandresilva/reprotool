47d0bbfcd3f134e3f07b785d43bdfa0d805f6eed
ofiala <ofiala@mujmail.cz>
2011-04-10 23:33:04 +0000

    Preference pages (head + lingtools). Head still contains sample fields.

---
+++ modules/reprotool.ide/src/reprotool/ide/preferences/PreferenceConstants.java
@@ -0,0 +1,18 @@
+package reprotool.ide.preferences;
+
+/**
+ * Constant definitions for plug-in preferences
+ */
+public class PreferenceConstants {
+
+	public static final String P_PATH = "pathPreference";
+
+	public static final String P_BOOLEAN = "booleanPreference";
+
+	public static final String P_CHOICE = "choicePreference";
+
+	public static final String P_STRING = "stringPreference";
+
+	public static final String MODEL_LOC = "modelLocation";
+	public static final String WORDNET_DICT = "wordnetDictionary";	
+}
