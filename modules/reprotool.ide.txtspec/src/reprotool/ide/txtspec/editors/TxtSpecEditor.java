0b7303248230b2b55435b27734c8c5b238952ef4
viliam.simko <viliam.simko@gmail.com>
2011-05-05 13:42:33 +0000

    Editor created from example

---
+++ modules/reprotool.ide.txtspec/src/reprotool/ide/txtspec/editors/TxtSpecEditor.java
@@ -0,0 +1,20 @@
+package reprotool.ide.txtspec.editors;
+
+import org.eclipse.ui.editors.text.TextEditor;
+
+public class TxtSpecEditor extends TextEditor {
+
+	private ColorManager colorManager;
+
+	public TxtSpecEditor() {
+		super();
+		colorManager = new ColorManager();
+		setSourceViewerConfiguration(new XMLConfiguration(colorManager));
+		setDocumentProvider(new XMLDocumentProvider());
+	}
+	public void dispose() {
+		colorManager.dispose();
+		super.dispose();
+	}
+
+}
