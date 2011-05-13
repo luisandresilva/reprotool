88b614d9f97d8c4674cc702c1ffdf9d99afb5687
viliam.simko <viliam.simko@gmail.com>
2011-05-13 10:32:20 +0000

    views of the Reprotool perspective are now defined in plugin.xml

--- modules/reprotool.ide/src/reprotool/ide/ReprotoolIdePerspectiveFactory.java
+++ modules/reprotool.ide/src/reprotool/ide/ReprotoolIdePerspectiveFactory.java
@@ -1,6 +1,5 @@
 package reprotool.ide;
 
-import org.eclipse.ui.IFolderLayout;
 import org.eclipse.ui.IPageLayout;
 import org.eclipse.ui.IPerspectiveFactory;
 
@@ -10,20 +9,7 @@ public class ReprotoolIdePerspectiveFactory implements IPerspectiveFactory {
 	}
 
 	public void createInitialLayout(IPageLayout myLayout) {
-		{
-			IFolderLayout folderLayout = myLayout.createFolder("folder", IPageLayout.BOTTOM, 0.66f, IPageLayout.ID_EDITOR_AREA);
-			folderLayout.addView("cz.cuni.mff.reprotool.ide.view_parsed_tree");
-			folderLayout.addView("cz.cuni.mff.reprotool.ide.views.view_linguistic_tools");
-			folderLayout.addView("org.eclipse.ui.views.ProgressView");
-		}
-		{
-			IFolderLayout folderLayout = myLayout.createFolder("folder_1", IPageLayout.RIGHT, 0.66f, IPageLayout.ID_EDITOR_AREA);
-			folderLayout.addView("cz.cuni.mff.reprotool.ide.uc_step_view");
-		}
-		{
-			IFolderLayout folderLayout = myLayout.createFolder("folder_2", IPageLayout.LEFT, 0.5f, IPageLayout.ID_EDITOR_AREA);
-			folderLayout.addView("org.eclipse.ui.navigator.ProjectExplorer");
-		}
+		// moved the definition of views to the Extensions section in the plugin.xml
 	}
 
 }
