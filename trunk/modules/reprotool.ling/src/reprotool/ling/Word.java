fdfdba08589c551ad02090517474467e45865f7d
ofiala@mujmail.cz <ofiala@mujmail.cz@7d00ebc3-3b19-6c51-bdff-128286bf8117>
2011-05-17 16:18:29 +0000

    modified ling objects + modified negations + added Wordl.java (should be there already for a week)

---
+++ trunk/modules/reprotool.ling/src/reprotool/ling/Word.java
@@ -0,0 +1,91 @@
+package reprotool.ling;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Pattern;
+
+import reprotool.ling.wordnet.WordNet;
+
+public class Word {
+	public String original = "";
+	public String text = "";
+	public String POS = "";
+	public Boolean interpunction = false;
+	public String negation = "";
+	
+	public Word() {
+		this.original = ""; 
+		this.text = "";  
+        this.POS = "";
+        this.interpunction = false;
+        this.negation = "";
+	}
+	
+	/**
+	 * Parse word string from PennTreeBank syntax
+	 *
+	 * @return void
+	 */		
+	public Word(String originalWord) {
+    	String[] parts = originalWord.split("_");
+    	List<String> antonyms = new ArrayList<String>();
+    	
+    	if(!parts[0].isEmpty()){
+    		this.original = originalWord;
+    		this.text = parts[0]; 
+    		this.interpunction = Pattern.matches("[,.!?]", this.text);
+			
+        	if(!parts[1].isEmpty()){
+        		this.POS = parts[1];  
+        	}
+    		
+    		String pos = "noun";
+    		if(this.POS == "IN")
+    			//pos = "adverb";
+    			pos = "adjective";
+ 
+    		antonyms = WordNet.getAntonyms(this.text, pos);
+    		if(!antonyms.isEmpty()){
+    			this.negation = antonyms.get(0);
+    		}
+    		
+
+    	}
+	}
+	
+	
+	/**
+	 * Parse word string from PennTreeBank syntax
+	 *
+	 * @return Boolean success
+	 */	
+    public Boolean parse(String originalWord) {
+    	String[] parts = originalWord.split("_");
+    	if(!parts[0].isEmpty()){
+    		this.original = originalWord;
+    		this.text = parts[0];  
+        	if(!parts[1].isEmpty()){
+        		this.POS = parts[1];  
+        		return true;
+        	}
+    	}
+    	return false;
+    }
+    
+	/**
+	 * Switch text and negation
+	 *
+	 * @return Boolean success
+	 */	
+    public Boolean negate() {
+    	if(!this.negation.isEmpty()){
+			String temp = this.text;
+			this.text = this.negation;
+			this.negation = temp;
+			return true;
+    	}
+    	return false;
+    }
+    
+    
+}
--- trunk/modules/reprotool.ling/src/reprotool/ling/wordnet/WordNet.java
+++ trunk/modules/reprotool.ling/src/reprotool/ling/wordnet/WordNet.java
@@ -5,6 +5,8 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.swing.JOptionPane;
+
 import org.eclipse.core.runtime.Platform;
 
 import edu.mit.jwi.Dictionary;
@@ -16,16 +18,21 @@ import edu.mit.jwi.item.POS;
 import edu.mit.jwi.item.Pointer;
 
 public class WordNet {
+	
+	private static IDictionary dict = null;
 
 	/**
 	 * Returns antonyms for given word
 	 *
 	 * @return List<String> antonyms 
 	 */
-    public static List<String> getAntonyms(String inWord) {	
+    public static List<String> get_Antonyms(String inWord) {	
 		
 		String path = Platform.getPreferencesService().getString("reprotool.ide", "wordnetDictionary", "/dict", null);
 		List<String> antonyms = new ArrayList<String>();
+		
+		if(inWord.isEmpty())
+			return antonyms;
 		
 		// check URL
     	URL url = null;
@@ -36,7 +43,8 @@ public class WordNet {
     	if(url == null) return antonyms;
     	
 		// construct the dictionary and open it
-		IDictionary dict = new Dictionary(url);
+		//IDictionary dict = new Dictionary(url);
+    	dict = new Dictionary(url);
     	try{ dict.open(); 
     	} catch(Exception e) { return antonyms; } 
 
@@ -69,7 +77,7 @@ public class WordNet {
     	catch(MalformedURLException e){ e.printStackTrace(); }
     	if(url == null) return false;
     	
-		IDictionary dict = new Dictionary(url);
+		dict = new Dictionary(url);
     	try{     	
     		// construct the dictionary and open it
     		dict.open(); 
@@ -87,4 +95,113 @@ public class WordNet {
     	return "testString";
     }
 
+	/**
+	 * Open if dictionary exists
+	 *
+	 * @return Boolean 
+	 */
+    public static Boolean open() {	
+
+		String path = Platform.getPreferencesService().getString("reprotool.ide", "wordnetDictionary", "/dict", null);
+   	
+		// check URL
+    	URL url = null;
+    	try{ 
+    		url = new URL("file", null, path); 
+    	} 
+    	catch(MalformedURLException e){ e.printStackTrace(); }
+    	if(url == null) return false;
+    	
+		dict = new Dictionary(url);
+    	try{     	
+    		// construct the dictionary and open it
+    		dict.open(); 
+    	} 
+    	catch(Exception e){ return false; }		
+
+		return true;			
+	}        
+
+	/**
+	 * Close if dictionary exists
+	 *
+	 * @return Boolean 
+	 */
+    public static Boolean close() {	
+    	
+    	if(dict.isOpen()) {
+    		dict.close();
+    		return true;
+    	}
+    	return false;	
+	}   
+ 
+    
+	/**
+	 * Returns antonyms for given word
+	 *
+	 * @return List<String> antonyms 
+	 */
+    public static List<String> getAntonyms(String inWord) {	
+		List<String> antonyms = new ArrayList<String>();
+	
+		if(inWord.isEmpty())			
+			return antonyms;
+
+		if(!dict.isOpen())
+			dict.open();		
+		
+		// look up first sense of the word
+		IIndexWord idxWord = dict.getIndexWord(inWord, POS.NOUN);
+		// bad err propagation in framework
+		if(idxWord != null){
+			IWordID wordID = idxWord.getWordIDs().get(0);
+			IWord word = dict.getWord(wordID);
+	
+			for (IWordID antonym : word.getRelatedWords(Pointer.ANTONYM)) {
+				antonyms.add(dict.getWord(antonym).getLemma());
+			} 
+		}
+		return antonyms;
+	}	  
+
+	/**
+	 * Returns antonyms for given word
+	 *
+	 * @return List<String> antonyms 
+	 */
+    public static List<String> getAntonyms(String inWord, String pos) {	
+		List<String> antonyms = new ArrayList<String>();
+	
+		if(inWord.isEmpty())			
+			return antonyms;
+
+		if(!dict.isOpen())
+			dict.open();	
+		
+		POS initPOS;
+		if(pos == "verb"){
+			initPOS = POS.VERB;			
+		} else if (pos == "adjective"){
+			initPOS = POS.ADJECTIVE;
+		} else if (pos == "adverb"){
+			initPOS = POS.ADVERB;
+		} else {
+			initPOS = POS.NOUN;
+		}
+				
+		// look up first sense of the word
+		IIndexWord idxWord = dict.getIndexWord(inWord, initPOS);
+		// bad err propagation in framework
+		if(idxWord != null){
+			IWordID wordID = idxWord.getWordIDs().get(0);
+			IWord word = dict.getWord(wordID);
+	
+			for (IWordID antonym : word.getRelatedWords(Pointer.ANTONYM)) {
+				antonyms.add(dict.getWord(antonym).getLemma());
+			} 
+		}
+		return antonyms;
+	}    
+    
 }
