e30e4ee16e6c4df70405574f3b2b7d098003a9a3
jiri.vinarek <jiri.vinarek@gmail.com>
2011-05-19 20:14:07 +0000

    project wizard fix and cleanup

--- modules/reprotool.ide/src/reprotool/ide/wizards/project/ProjectWizard.java
+++ modules/reprotool.ide/src/reprotool/ide/wizards/project/ProjectWizard.java
@@ -8,16 +8,13 @@ import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IProjectDescription;
 import org.eclipse.core.resources.IWorkspace;
 import org.eclipse.core.resources.ResourcesPlugin;
-import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.core.runtime.Path;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
-import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.viewers.StructuredSelection;
@@ -33,52 +30,27 @@ import org.eclipse.ui.dialogs.WizardNewProjectCreationPage;
 import org.eclipse.ui.part.FileEditorInput;
 import org.eclipse.ui.part.ISetSelectionTarget;
 
-import org.eclipse.core.internal.resources.File;
-
 import reprotool.ide.service.Service;
 
 public class ProjectWizard extends Wizard implements INewWizard {
 
+	private static final String PROJECT_FILE_NAME = "project.ucproj";
 	private static final String WIZARD_PAGE_TITLE = "New Reprotool Project";
 	private static final String WIZARD_PAGE_DESCRIPTION = "Create a new Reprotool Project.";
 	private static final String WIZARD_PAGE_NAME = "NewReprotoolProject";
-	private IWorkbench workbench;
-	private IStructuredSelection selection;
-	WizardNewProjectCreationPage wizardPage;
 
-	public ProjectWizard() {
-		// TODO Auto-generated constructor stub
-	}
+	private WizardNewProjectCreationPage wizardPage;
+	private IWorkbench workbench;
 
 	@Override
 	public void init(IWorkbench workbench, IStructuredSelection selection) {
 		this.workbench = workbench;
-		this.selection = selection;
-		// TODO - jvinarek - add title and banner
 		setWindowTitle("New Reprotool project");
-		// setWindowTitle(MessageUtil.getString("New_Readme_File"));
-		// setDefaultPageImageDescriptor(ReadmeImages.README_WIZARD_BANNER);
-
 	}
 
 	@Override
 	public boolean performFinish() {
-
-		// test
-		// final IProject projectHandle = wizardPage.getProjectHandle();
-		// java.net.URI projectURI = (!wizardPage.useDefaults()) ? wizardPage
-		// .getLocationURI() : null;
-		// IWorkspace workspace = ResourcesPlugin.getWorkspace();
-		// final IProjectDescription desc = workspace
-		// .newProjectDescription(projectHandle.getName());
-		// desc.setLocationURI(projectURI);
-		// test - end
-
 		try {
-			// Remember the file.
-			//
-//			final IFile modelFile = getModelFile();
-
 			// Do the work within an operation.
 			//
 			WorkspaceModifyOperation operation = new WorkspaceModifyOperation() {
@@ -88,14 +60,15 @@ public class ProjectWizard extends Wizard implements INewWizard {
 						// Create project
 						//
 						IProject projectHandle = wizardPage.getProjectHandle();
-						java.net.URI projectURI = (!wizardPage.useDefaults()) ? wizardPage.getLocationURI() : null;
-						// java.net.URI projectURI =
-						// wizardPage.getLocationURI();
+						java.net.URI projectURI = wizardPage.useDefaults() ? null
+								: wizardPage.getLocationURI();
 
 						IWorkspace workspace = ResourcesPlugin.getWorkspace();
 
-						// TODO - jvinarek description
-						IProjectDescription desc = workspace.newProjectDescription(projectHandle.getName());
+						// TODO - jvinarek - add project description, not only
+						// project name
+						IProjectDescription desc = workspace
+								.newProjectDescription(projectHandle.getName());
 						desc.setLocationURI(projectURI);
 
 						projectHandle.create(desc, progressMonitor);
@@ -106,20 +79,9 @@ public class ProjectWizard extends Wizard implements INewWizard {
 
 						// Get the URI of the model file.
 						//
-						// URI fileURI1 =
-						// URI.createPlatformResourceURI(modelFile
-						// .getFullPath().toString(), true);
-						// URI test1 =
-						// URI.createFileURI(modelFile.getFullPath().toString());
-						// URI fileURI =
-						// URI.createFileURI("c:\\tmp\\xx.ucproj");
-						URI fileURI = URI.createFileURI(projectHandle.getLocation().toString() + "/project.ucproj");
-
-						// URI fileURI = URI.createURI(projectURI.toString()
-						// + "\\" + "project.ucproj");
-
-						// URI fileURI = URI.createURI(projectURI.toString()
-						// + "\\" + "project.ucproj");
+						URI fileURI = URI.createFileURI(projectHandle
+								.getLocation().append(PROJECT_FILE_NAME)
+								.toString());
 
 						// Create a resource for this file.
 						//
@@ -135,12 +97,10 @@ public class ProjectWizard extends Wizard implements INewWizard {
 						// Save the contents of the resource to the file system.
 						//
 						Map<Object, Object> options = new HashMap<Object, Object>();
-						// TODO - jvinarek - add encoding ?
-						// options.put(XMLResource.OPTION_ENCODING,
-						// initialObjectCreationPage.getEncoding());
 						resource.save(options);
-						
-						// open project
+
+						// Open project
+						//
 						projectHandle.open(progressMonitor);
 					} catch (Exception exception) {
 						// TODO - jvinarek - log
@@ -154,37 +114,44 @@ public class ProjectWizard extends Wizard implements INewWizard {
 
 			getContainer().run(false, false, operation);
 
+			IFile modelFile = ResourcesPlugin.getWorkspace().getRoot()
+					.getFile(getModelFilePath());
+
 			// Select the new file resource in the current view.
-//
-//			File modelFile = null;
-//			
-//			IWorkbenchWindow workbenchWindow = workbench.getActiveWorkbenchWindow();
-//			IWorkbenchPage page = workbenchWindow.getActivePage();
-//			final IWorkbenchPart activePart = page.getActivePart();
-//			if (activePart instanceof ISetSelectionTarget) {
-//				final ISelection targetSelection = new StructuredSelection(modelFile);
-//				getShell().getDisplay().asyncExec(new Runnable() {
-//					public void run() {
-//						((ISetSelectionTarget) activePart).selectReveal(targetSelection);
-//					}
-//				});
-//			}
-//
-//			// Open an editor on the new file.
-//			//
-//			try {
-//				page.openEditor(new FileEditorInput(modelFile),
-//						workbench.getEditorRegistry().getDefaultEditor(modelFile.getFullPath().toString()).getId());
-//			} catch (PartInitException exception) {
-//				// TODO - jvinarek - open error dialog
-//				MessageDialog.openError(workbenchWindow.getShell(), "Open editor problem", exception.getMessage());
-//				// MessageDialog.openError(workbenchWindow.getShell(),
-//				//
-//				//
-//				// ReprotoolEditorPlugin.INSTANCE.getString("_UI_OpenEditorError_label"),
-//				// exception.getMessage());
-//				return false;
-//			}
+			//
+			IWorkbenchWindow workbenchWindow = workbench
+					.getActiveWorkbenchWindow();
+			IWorkbenchPage page = workbenchWindow.getActivePage();
+			final IWorkbenchPart activePart = page.getActivePart();
+			if (activePart instanceof ISetSelectionTarget) {
+				final ISelection targetSelection = new StructuredSelection(
+						modelFile);
+				getShell().getDisplay().asyncExec(new Runnable() {
+					public void run() {
+						((ISetSelectionTarget) activePart)
+								.selectReveal(targetSelection);
+					}
+				});
+			}
+
+			// Open an editor on the new file.
+			//
+			try {
+				page.openEditor(
+						new FileEditorInput(modelFile),
+						workbench
+								.getEditorRegistry()
+								.getDefaultEditor(
+										modelFile.getFullPath().toString())
+								.getId());
+			} catch (PartInitException exception) {
+				// TODO - jvinarek - messagebox
+				// MessageDialog.openError(workbenchWindow.getShell(),
+				// ReprotoolModelEditorPlugin.INSTANCE.getString("_UI_OpenEditorError_label"),
+				// exception.getMessage());
+				exception.printStackTrace();
+				return false;
+			}
 
 			return true;
 		} catch (Exception exception) {
@@ -192,21 +159,6 @@ public class ProjectWizard extends Wizard implements INewWizard {
 			exception.printStackTrace();
 			return false;
 		}
-	}
-
-	private IFile getModelFile(URI fileUri) {
-		// IPath path = wizardPage.getLocationPath();
-		// IPath path = wizardPage.getProjectHandle().getFullPath();
-		IPath path = new Path("c:\\tmp\\zzz.test");
-		String projectFileName = "project.ucproj";
-		// IFile result =
-		// ResourcesPlugin.getWorkspace().getRoot().getFile(path.append(projectFileName));
-		// test
-		IFile result = ResourcesPlugin.getWorkspace().getRoot().getFile(path);
-		return result;
-		
-//		IFile file = new File
-//		return 
 	}
 
 	@Override
@@ -217,18 +169,14 @@ public class ProjectWizard extends Wizard implements INewWizard {
 		addPage(wizardPage);
 	}
 
-	/**
-	 * Get the file from the page. <!-- begin-user-doc --> <!-- end-user-doc -->
-	 * 
-	 * @generated
-	 */
-	// public IFile getModelFile() {
-	// return wizardPage.getModelFile();
-	// }
-
-	// TODO - jvinarek - change
 	private EObject createInitialModel() {
 		return Service.INSTANCE.createSoftwareProject();
+	}
+
+	private IPath getModelFilePath() {
+		IProject projectHandle = wizardPage.getProjectHandle();
+		return projectHandle.getProjectRelativePath()
+				.append(projectHandle.getName()).append(PROJECT_FILE_NAME);
 	}
 
 }
