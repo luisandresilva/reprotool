0cd6cf01ca7ea46c1a0b63d6beecdc2738d92539
jiri.vinarek <jiri.vinarek@gmail.com>
2011-05-26 23:13:23 +0000

    project explorer - "actors" node showed only in projects with reprotool nature.

--- modules/reprotool.ide/src/reprotool/ide/views/ActorsContentProvider.java
+++ modules/reprotool.ide/src/reprotool/ide/views/ActorsContentProvider.java
@@ -1,13 +1,22 @@
 package reprotool.ide.views;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.HashMap;
+
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.emf.ecore.xmi.impl.XMIResourceImpl;
 import org.eclipse.jface.viewers.ITreeContentProvider;
 import org.eclipse.jface.viewers.Viewer;
 
+import reprotool.ide.natures.ReprotoolProjectNature;
 import reprotool.ide.service.Service;
 import reprotool.model.swproj.Actor;
 import reprotool.model.swproj.SoftwareProject;
+import reprotool.model.swproj.impl.SoftwareProjectImpl;
+import reprotool.model.swproj.impl.SwprojFactoryImpl;
 
 public class ActorsContentProvider implements ITreeContentProvider {
 
@@ -17,6 +26,8 @@ public class ActorsContentProvider implements ITreeContentProvider {
 	// private static final Object[] DUMMY_ACTORS = new String[] { "Clerk",
 	// "Account manager", "Bank information system"};
 
+	private SoftwareProject[] swProjectModelArray;
+	
 	// TODO - jvinarek - mock only
 	private static final SoftwareProject[] PROJECT = new SoftwareProject[] { Service.INSTANCE.getSoftwareProject() };
 
@@ -41,12 +52,39 @@ public class ActorsContentProvider implements ITreeContentProvider {
 	public Object[] getChildren(Object parentElement) {
 		if (parentElement instanceof IProject) {
 			// TODO - jvinarek - test only, change
-			return PROJECT;
+			IProject project = (IProject)parentElement;
+			try {
+				if (project.hasNature(ReprotoolProjectNature.NATURE_ID)) {
+					if (swProjectModelArray == null) {
+						swProjectModelArray = loadSwProjectModel(project);
+					}
+					return swProjectModelArray; 
+				} else {
+					return NO_CHILDREN;
+				}
+			} catch (CoreException e) {
+				// TODO - jvinarek - log
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO - jvinarek - log
+				e.printStackTrace();
+			}
 		}
 		if (parentElement instanceof SoftwareProject) {
-			((SoftwareProject)parentElement).getActors().toArray(); 
+			return ((SoftwareProject)parentElement).getActors().toArray(); 
 		}
 		return NO_CHILDREN;
+	}
+
+	private SoftwareProject[] loadSwProjectModel(IProject project) throws IOException, CoreException {
+		XMIResourceImpl resource = new XMIResourceImpl();
+		IFile file = project.getFile("project.ucproj");
+		
+		HashMap<Object, Object> options = new HashMap<Object, Object>(); 
+		resource.load(file.getContents(), options);
+		SoftwareProject softwareProject = (SoftwareProject)resource.getContents().get(0);
+		
+		return new SoftwareProject[] { softwareProject };
 	}
 
 	@Override
