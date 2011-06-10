50920ab13a2e5b820dbd5d4e75a125e38413a761
jiri.vinarek@gmail.com <jiri.vinarek@gmail.com@7d00ebc3-3b19-6c51-bdff-128286bf8117>
2011-06-10 11:26:01 +0000

    Removed obsolete ProjectEditor, change Remove actor command for use in project explorer popup menu

--- trunk/modules/reprotool.ide/src/reprotool/ide/commands/RemoveActor.java
+++ trunk/modules/reprotool.ide/src/reprotool/ide/commands/RemoveActor.java
@@ -1,48 +1,53 @@
 package reprotool.ide.commands;
 
+import java.io.IOException;
+import java.util.Collections;
+import java.util.Iterator;
+
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.ui.IPageLayout;
 import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.handlers.HandlerUtil;
 
-import reprotool.ide.views.ProjectView;
 import reprotool.model.swproj.Actor;
-
-
+import reprotool.model.swproj.SoftwareProject;
 
 /**
- * Command to remove selected actor. Proof of concept.
+ * Command to remove selected actor from project. Used in project explorer.
  * 
  * @author jvinarek
- *
+ * 
  */
 public class RemoveActor extends AbstractHandler {
 
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
-		// get selected actor
 		IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindow(event);
 		IWorkbenchPage page = window.getActivePage();
-		ProjectView view = (ProjectView) page.findView(ProjectView.ID);
-		IStructuredSelection selection = (IStructuredSelection)view.getTreeViewerActors().getSelection();
+		// //the current selection in the project explorer
+		IStructuredSelection selection = (IStructuredSelection) page.getSelection(IPageLayout.ID_PROJECT_EXPLORER);
+
 		if (!selection.isEmpty()) {
-			Actor actor = (Actor)selection.getFirstElement();
-			
-			// remove actor from project
-			view.getProject().getActors().remove(actor);
-//			Actor actor = (Actor)selection.iterator().next();
-			
-			// remove from model
-//			ModelProvider.INSTANCE.getActors().remove(actor);
-	
-			// reload view
-//			view.getTreeViewerActors().refresh();
+			Iterator iter = selection.iterator();
+			SoftwareProject project = (SoftwareProject) ((Actor) selection.getFirstElement()).eContainer();
+
+			while (iter.hasNext()) {
+				Actor actor = (Actor) iter.next();
+				project.getActors().remove(actor);
+			}
+
+			try {
+				project.eResource().save(Collections.EMPTY_MAP);
+			} catch (IOException e) {
+				// TODO - jvinarek - add logging
+				e.printStackTrace();
+			}
 		}
 
 		return null;
 	}
-
 }
