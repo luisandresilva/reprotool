6905c6932ca1e54abca45d0367eb998ec2d7ff27
jankrajicek@gmail.com <jankrajicek@gmail.com@7d00ebc3-3b19-6c51-bdff-128286bf8117>
2011-06-13 13:35:03 +0000

    scenario type (extension/variation) can be changed from the properties

--- trunk/modules/reprotool.ide/src/reprotool/ide/adapter/ScenarioPropertySource.java
+++ trunk/modules/reprotool.ide/src/reprotool/ide/adapter/ScenarioPropertySource.java
@@ -1,13 +1,17 @@
 package reprotool.ide.adapter;
 
+import org.eclipse.ui.views.properties.ComboBoxPropertyDescriptor;
 import org.eclipse.ui.views.properties.IPropertyDescriptor;
 import org.eclipse.ui.views.properties.IPropertySource;
 import org.eclipse.ui.views.properties.PropertyDescriptor;
 
+import reprotool.ide.editors.UseCaseEditor;
 import reprotool.model.usecase.Scenario;
+import reprotool.model.usecase.UseCaseStep;
 
 public class ScenarioPropertySource implements IPropertySource {
 	private static final String ID_DESCRIPTION = "description";
+	private static final String ID_TYPE = "type";
 	
 	private Scenario scen;
 	private IPropertyDescriptor[] propertyDescriptors;
@@ -25,7 +29,8 @@ public class ScenarioPropertySource implements IPropertySource {
 	public IPropertyDescriptor[] getPropertyDescriptors() {
 		if (propertyDescriptors == null) {
             IPropertyDescriptor desc = new PropertyDescriptor(ID_DESCRIPTION, "Description");
-            propertyDescriptors = new IPropertyDescriptor[] { desc };
+            IPropertyDescriptor type = new ComboBoxPropertyDescriptor(ID_TYPE, "Scenario type", new String[] {"variation", "extension"});
+            propertyDescriptors = new IPropertyDescriptor[] { desc, type };
 	    }
 		return propertyDescriptors;
 	}
@@ -37,6 +42,9 @@ public class ScenarioPropertySource implements IPropertySource {
 				return "";
 			else
 				return scen.getDescription();
+		} else if (id.equals(ID_TYPE)) {
+			UseCaseStep parent = (UseCaseStep)scen.eContainer();
+			return parent.getExtension().contains(scen) ? 1:0;
 		}
 		return null;
 	}
@@ -52,6 +60,23 @@ public class ScenarioPropertySource implements IPropertySource {
 
 	@Override
 	public void setPropertyValue(Object id, Object value) {
+		if (getPropertyValue(id).equals(value))
+			return;
+		UseCaseEditor editor = UseCaseEditor.getActiveUseCaseEditor();
+		if (id.equals(ID_TYPE)) {
+			boolean setVariation = value.equals(0);
+			editor.saveUndoState();
+			editor.setDirty();
+			UseCaseStep parent = (UseCaseStep)scen.eContainer();
+			if (setVariation) {
+				parent.getExtension().remove(scen);
+				parent.getVariation().add(scen);
+			} else {
+				parent.getVariation().remove(scen);
+				parent.getExtension().add(scen);
+			}
+			editor.refresh();
+		}
 	}
 
 }
