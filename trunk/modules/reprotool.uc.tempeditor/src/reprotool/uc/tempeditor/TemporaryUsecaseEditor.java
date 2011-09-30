5cfc23bb91239aeb1b61e6b8146b26782837ec1b
rudotx <rudotx@7d00ebc3-3b19-6c51-bdff-128286bf8117>
2011-09-30 08:58:24 +0000

    generation of LTS model from UseCase Added a temporary demo editor for usecases that inherits from generated editor and shows simple LTS in outline (extensions and variations are supported)

--- trunk/modules/reprotool.uc.tempeditor/src/reprotool/uc/tempeditor/TemporaryUsecaseEditor.java
+++ trunk/modules/reprotool.uc.tempeditor/src/reprotool/uc/tempeditor/TemporaryUsecaseEditor.java
@@ -1,25 +1,28 @@
-package reprotool.lts.editor;
+package reprotool.uc.tempeditor;
 
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.ISelectionChangedListener;
 import org.eclipse.jface.viewers.SelectionChangedEvent;
 import org.eclipse.ui.views.contentoutline.IContentOutlinePage;
-import reprotool.model.lts.StateMachine;
-import reprotool.model.lts.presentation.LtsEditor;
 
-/**
- * This is extended LTS editor build upon the generated LtsEditor
- */
-public class ExtendedLtsEditor extends LtsEditor {
+import reprotool.model.lts.StateMachine;
+import reprotool.model.usecase.Scenario;
+import reprotool.model.usecase.UseCase;
+import reprotool.model.usecase.presentation.UsecaseEditor;
+
+public class TemporaryUsecaseEditor extends UsecaseEditor {
 	
 	@Override
 	public IContentOutlinePage getContentOutlinePage() {
 		if (contentOutlinePage == null) {
 			Resource resource = editingDomain.getResourceSet().getResources().get(0);
 			Object obj = resource.getContents().get(0);
-			StateMachine machine = (StateMachine) obj;			
-
+			UseCase uc = (UseCase) obj;
+			Scenario s = uc.getMainScenario();
+			
+			LTSGenerator generator = new LTSGenerator(s);
+			StateMachine machine = generator.getLabelTransitionSystem();
 			contentOutlinePage = new LTSContentOutlinePage(machine);
 			
 			// Listen to selection so that we can handle it is a special way.
@@ -37,9 +40,10 @@ public class ExtendedLtsEditor extends LtsEditor {
 		return contentOutlinePage;
 	}
 	
+	
 	@Override
 	public void handleContentOutlineSelection(ISelection selection) {
 		/* Not implemented */
 	}
-	
+
 }
