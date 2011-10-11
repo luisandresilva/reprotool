7d77c6a83afaad57d461602a30b7cadfcf1c7b7c
rudotx <rudotx@7d00ebc3-3b19-6c51-bdff-128286bf8117>
2011-10-11 18:56:06 +0000

    work in progress - showing included use-cases in the outline

--- modules/reprotool.uc.tempeditor/src/reprotool/uc/tempeditor/FigureProvider.java
+++ modules/reprotool.uc.tempeditor/src/reprotool/uc/tempeditor/FigureProvider.java
@@ -65,6 +65,30 @@ public class FigureProvider {
 		return fig;
 	}
 	
+	private ImageFigure drawSuccessStateFigure() {
+		PaletteData pData = new PaletteData(255, 255, 255);
+		ImageData sourceData = new ImageData(STATE_NODE_DIAMETER, STATE_NODE_DIAMETER, 8, pData);
+
+		Image image = new Image(Display.getDefault(), sourceData);
+		GC gc = new GC(image);
+		gc.setAdvanced(true);
+		gc.setAntialias(SWT.ON);
+		
+		gc.setBackground(new Color(Display.getDefault(), 255, 255, 255));
+		gc.fillRectangle(0, 0, image.getBounds().width, image.getBounds().height);
+
+		gc.setBackground(new Color(Display.getDefault(), 0, 255, 0));
+		gc.fillOval(0, 0, image.getBounds().width, image.getBounds().height);
+
+		gc.dispose();
+		
+		ImageFigure fig = new ImageFigure();
+		fig.setImage(image);
+		fig.setSize(image.getBounds().width, image.getBounds().height);
+		
+		return fig;
+	}
+	
 	private IFigure drawNormalStateFigure() {
 		PaletteData pData = new PaletteData(255, 255, 255);
 		ImageData sourceData = new ImageData(STATE_NODE_DIAMETER, STATE_NODE_DIAMETER, 8, pData);
@@ -95,6 +119,8 @@ public class FigureProvider {
 			return drawInitialStateFigure();
 		} else if (s == machine.getAbortState()) {
 			return drawAbortStateFigure();
+		} else if (s == machine.getSuccessState()) {
+			return drawSuccessStateFigure();
 		} else {
 			return drawNormalStateFigure();
 		}
