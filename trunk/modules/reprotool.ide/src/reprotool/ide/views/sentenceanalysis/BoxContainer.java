9ed2233ba303a3dcb2102b1cce1d8c2cc1b8f56c
jiri.vinarek@gmail.com <jiri.vinarek@gmail.com@7d00ebc3-3b19-6c51-bdff-128286bf8117>
2011-10-31 22:33:07 +0000

    fixed layout of the ActionPart boxes in sentence analysis view.

--- trunk/modules/reprotool.ide/src/reprotool/ide/views/sentenceanalysis/BoxContainer.java
+++ trunk/modules/reprotool.ide/src/reprotool/ide/views/sentenceanalysis/BoxContainer.java
@@ -1,8 +1,8 @@
 package reprotool.ide.views.sentenceanalysis;
 
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.custom.SashForm;
-import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.layout.RowData;
+import org.eclipse.swt.layout.RowLayout;
 import org.eclipse.swt.widgets.Composite;
 
 /**
@@ -19,7 +19,6 @@ public class BoxContainer extends Composite {
 	private TextWithComboBox receiverBox;
 	private TextWithComboBox actionParamBox;
 	private TextWithComboBox senderBox;
-	private SashForm sashForm;
 
 	/**
 	 * Create the composite.
@@ -29,30 +28,37 @@ public class BoxContainer extends Composite {
 	 */
 	public BoxContainer(Composite parent, int style) {
 		super(parent, style);
-		setLayout(new FillLayout(SWT.HORIZONTAL));
+		RowLayout rowLayout = new RowLayout(SWT.HORIZONTAL);
+		rowLayout.fill = true;
+		setLayout(rowLayout);
 
-		sashForm = new SashForm(this, SWT.NONE);
+		actionTypeBox = new ActionTypeBox(this, SWT.NONE);
 
-		actionTypeBox = new ActionTypeBox(sashForm, SWT.NONE);
-
-		senderBox = new TextWithComboBox(sashForm, SWT.NONE);
+		senderBox = new TextWithComboBox(this, SWT.NONE);
 		senderBox.getGrpActionPart().setText("Sender");
 
-		activityBox = new ActivityBox(sashForm, SWT.NONE);
+		activityBox = new ActivityBox(this, SWT.NONE);
+		// TODO - jvinarek - temporarily excluded
+		RowData rd_activityBox = new RowData(SWT.DEFAULT, SWT.DEFAULT);
+		rd_activityBox.exclude = true;
+		activityBox.setLayoutData(rd_activityBox);
 		activityBox.getLblMarkedText().setText("Marked activity");
 
-		actionParamBox = new TextWithComboBox(sashForm, SWT.NONE);
+		actionParamBox = new TextWithComboBox(this, SWT.NONE);
+		// TODO - jvinarek - temporarily excluded
+		RowData rd_actionParamBox = new RowData(SWT.DEFAULT, SWT.DEFAULT);
+		rd_actionParamBox.exclude = true;
+		actionParamBox.setLayoutData(rd_actionParamBox);
 		actionParamBox.getGrpActionPart().setText("Action parameter");
 
-		receiverBox = new TextWithComboBox(sashForm, SWT.NONE);
+		receiverBox = new TextWithComboBox(this, SWT.NONE);
 		receiverBox.getGrpActionPart().setText("Receiver");
 
-		gotoUseCaseStepBox = new TextWithComboBox(sashForm, SWT.NONE);
+		gotoUseCaseStepBox = new TextWithComboBox(this, SWT.NONE);
 		gotoUseCaseStepBox.getGrpActionPart().setText("Goto target");
 
-		includeUseCaseBox = new TextWithComboBox(sashForm, SWT.NONE);
+		includeUseCaseBox = new TextWithComboBox(this, SWT.NONE);
 		includeUseCaseBox.getGrpActionPart().setText("Include use case");
-		sashForm.setWeights(new int[] { 1, 1, 1, 1, 1, 1, 1 });
 	}
 
 	@Override
@@ -88,7 +94,4 @@ public class BoxContainer extends Composite {
 		return senderBox;
 	}
 
-	public SashForm getSashForm() {
-		return sashForm;
-	}
 }
