43b6365c184dbec6579a32f92c220484c9f86823
viliam.simko@gmail.com <viliam.simko@gmail.com@7d00ebc3-3b19-6c51-bdff-128286bf8117>
2011-11-05 08:46:39 +0000

    improved temporal annotations

---
+++ trunk/modules/reprotool.model/src/reprotool/model/usecase/annotate/impl/TemporalAnnotationGroupImplCustom.java
@@ -0,0 +1,21 @@
+package reprotool.model.usecase.annotate.impl;
+
+import java.util.List;
+
+import org.apache.commons.lang.StringUtils;
+import org.eclipse.xtext.EcoreUtil2;
+
+import reprotool.model.usecase.annotate.AnnotatePackage;
+
+/**
+ * @author Viliam Simko
+ */
+public class TemporalAnnotationGroupImplCustom extends TemporalAnnotationGroupImpl
+{
+	@Override
+	public String getDescription()
+	{
+		List<String> descrList = EcoreUtil2.collect(getMembers(), AnnotatePackage.STEP_ANNOTATION_TYPE__NAME, String.class);
+		return StringUtils.join( descrList, "-");
+	}
+}
