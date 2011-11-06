1d7c38e3ce4db92d7d6d4e171d3ede1f647f11c7
jiri.vinarek@gmail.com <jiri.vinarek@gmail.com@7d00ebc3-3b19-6c51-bdff-128286bf8117>
2011-11-06 19:15:06 +0000

    fixed deleting of the use cases.

--- trunk/modules/reprotool.model/src/reprotool/model/usecase/impl/UseCaseImplCustom.java
+++ trunk/modules/reprotool.model/src/reprotool/model/usecase/impl/UseCaseImplCustom.java
@@ -1,6 +1,5 @@
 package reprotool.model.usecase.impl;
 
-import org.eclipse.emf.common.util.BasicEList;
 import org.eclipse.emf.common.util.EList;
 
 import reprotool.model.usecase.Scenario;
@@ -14,15 +13,13 @@ public class UseCaseImplCustom extends UseCaseImpl {
 	}
 	
 	private EList<UseCaseStep> getStepsFromScenario(Scenario scenario) {
-		EList<UseCaseStep> list = new BasicEList<UseCaseStep>();  
-		
-		// check is needed, exception was thrown when deleting use case or actor
-		if (scenario != null) {
-			list.addAll(scenario.getSteps());
-			// TODO - jvinarek - add recursion, get steps from extensions & variations. 
+		// check is needed, exception was thrown when deleting actor
+		if (scenario == null) {
+			return null;
 		}
 		
-		return list;
+		// TODO - jvinarek - add recursion, get steps from extensions & variations. 
+		return scenario.getSteps();
 	}
 	
 }
