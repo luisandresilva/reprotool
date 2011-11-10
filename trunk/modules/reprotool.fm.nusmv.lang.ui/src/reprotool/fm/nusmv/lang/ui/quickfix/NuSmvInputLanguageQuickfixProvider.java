a6c7d46be8ba62520f12b881f7131b9bad76d7c0
viliam.simko@gmail.com <viliam.simko@gmail.com@7d00ebc3-3b19-6c51-bdff-128286bf8117>
2011-11-10 13:32:21 +0000

    added initial implementation of: - validation (mandatory "main" module ...) - quickfix (add main module, remove params from main module ...)

--- trunk/modules/reprotool.fm.nusmv.lang.ui/src/reprotool/fm/nusmv/lang/ui/quickfix/NuSmvInputLanguageQuickfixProvider.java
+++ trunk/modules/reprotool.fm.nusmv.lang.ui/src/reprotool/fm/nusmv/lang/ui/quickfix/NuSmvInputLanguageQuickfixProvider.java
@@ -1,19 +1,74 @@
 
 package reprotool.fm.nusmv.lang.ui.quickfix;
 
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.jface.text.BadLocationException;
+import org.eclipse.xtext.ui.editor.model.IXtextDocument;
+import org.eclipse.xtext.ui.editor.model.edit.IModification;
+import org.eclipse.xtext.ui.editor.model.edit.IModificationContext;
+import org.eclipse.xtext.ui.editor.model.edit.ISemanticModification;
 import org.eclipse.xtext.ui.editor.quickfix.DefaultQuickfixProvider;
+import org.eclipse.xtext.ui.editor.quickfix.Fix;
+import org.eclipse.xtext.ui.editor.quickfix.IssueResolutionAcceptor;
+import org.eclipse.xtext.validation.Issue;
+
+import reprotool.fm.nusmv.lang.nuSmvInputLanguage.Model;
+import reprotool.fm.nusmv.lang.nuSmvInputLanguage.Module;
+import reprotool.fm.nusmv.lang.validation.NuSmvInputLanguageJavaValidator;
 
 public class NuSmvInputLanguageQuickfixProvider extends DefaultQuickfixProvider {
 
-//	@Fix(MyJavaValidator.INVALID_NAME)
-//	public void capitalizeName(final Issue issue, IssueResolutionAcceptor acceptor) {
-//		acceptor.accept(issue, "Capitalize name", "Capitalize the name.", "upcase.png", new IModification() {
-//			public void apply(IModificationContext context) throws BadLocationException {
-//				IXtextDocument xtextDocument = context.getXtextDocument();
-//				String firstLetter = xtextDocument.get(issue.getOffset(), 1);
-//				xtextDocument.replace(issue.getOffset(), 1, firstLetter.toUpperCase());
-//			}
-//		});
-//	}
+	@Fix(NuSmvInputLanguageJavaValidator.MISSING_MAIN_MODULE)
+	public void addMainModule(final Issue issue, IssueResolutionAcceptor acceptor) {
+		acceptor.accept(
+			issue,
+			"Add main module",
+			"This will add MODULE main to the file",
+			"Operation.gif",
+			new IModification() {
+				public void apply(IModificationContext context) throws BadLocationException {
+					IXtextDocument xtextDocument = context.getXtextDocument();
+					xtextDocument.set("MODULE main\n\n" + xtextDocument.get());
+				}
+			}
+		);
+	}
+
+	@Fix(NuSmvInputLanguageJavaValidator.MISSING_MAIN_MODULE)
+	public void renameFirstModule(final Issue issue, IssueResolutionAcceptor acceptor) {
+		acceptor.accept(
+			issue,
+			"Rename first module to 'main'",
+			"This will rename the first module in your code",
+			"Operation.gif",
+			new ISemanticModification() {
+				
+				public void apply(EObject element, IModificationContext context)
+						throws Exception {
+					Model model = (Model) element;
+					Module firstModule = model.getModules().get(0);
+					firstModule.setName("main");
+				}
+			}
+		);
+	}
+	
+	@Fix(NuSmvInputLanguageJavaValidator.PARAMETERS_NOT_ALLOWED)
+	public void removeModuleParams(final Issue issue, IssueResolutionAcceptor acceptor) {
+		acceptor.accept(
+			issue,
+			"Remove parameters",
+			"Parameters will be removed from the module",
+			"Operation.gif",
+			new ISemanticModification() {
+				public void apply(EObject element, IModificationContext context)
+						throws Exception {
+					Module module = (Module) element;
+					module.getParams().clear();
+				}
+			}
+		);
+	}
+	
 
 }
--- trunk/modules/reprotool.fm.nusmv.lang/src/reprotool/fm/nusmv/lang/validation/NuSmvInputLanguageJavaValidator.java
+++ trunk/modules/reprotool.fm.nusmv.lang/src/reprotool/fm/nusmv/lang/validation/NuSmvInputLanguageJavaValidator.java
@@ -1,13 +1,30 @@
 package reprotool.fm.nusmv.lang.validation;
+
+import org.eclipse.xtext.validation.Check;
+
+import reprotool.fm.nusmv.lang.nuSmvInputLanguage.Model;
+import reprotool.fm.nusmv.lang.nuSmvInputLanguage.Module;
+import reprotool.fm.nusmv.lang.nuSmvInputLanguage.NuSmvInputLanguagePackage;
  
 
 public class NuSmvInputLanguageJavaValidator extends AbstractNuSmvInputLanguageJavaValidator {
 
-//	@Check
-//	public void checkGreetingStartsWithCapital(Greeting greeting) {
-//		if (!Character.isUpperCase(greeting.getName().charAt(0))) {
-//			warning("Name should start with a capital", MyDslPackage.Literals.GREETING__NAME);
-//		}
-//	}
+	static final public String MISSING_MAIN_MODULE = "NuSmvInputLanguageJavaValidator.1";
+	static final public String PARAMETERS_NOT_ALLOWED = "NuSmvInputLanguageJavaValidator.2";
+	
+	@Check
+	public void checkModelContainsMainModule(Model nusmvModel) {
+		for (Module module : nusmvModel.getModules()) {
+			if(module.getName().equals("main"))
+				return;
+		}
+		error("Main module is missing", NuSmvInputLanguagePackage.Literals.MODEL__MODULES, MISSING_MAIN_MODULE);
+	}
+
+	@Check
+	public void checkMainModuleWithoutParams(Module module) {
+		if(module.getName().equals("main") && !module.getParams().isEmpty())
+			error("Parameters in main module are not allowed", NuSmvInputLanguagePackage.Literals.MODULE__PARAMS, PARAMETERS_NOT_ALLOWED);
+	}
 
 }
