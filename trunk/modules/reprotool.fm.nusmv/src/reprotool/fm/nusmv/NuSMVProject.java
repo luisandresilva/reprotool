b7b990f7ad9fad22480b7a800d14199319e94e24
rudotx <rudotx@7d00ebc3-3b19-6c51-bdff-128286bf8117>
2011-11-12 16:25:57 +0000

    converting project to nusmv - added handling of ctl specifications some code cleanup and refactoring

--- trunk/modules/reprotool.fm.nusmv/src/reprotool/fm/nusmv/NuSMVProject.java
+++ trunk/modules/reprotool.fm.nusmv/src/reprotool/fm/nusmv/NuSMVProject.java
@@ -5,6 +5,20 @@ import java.util.HashMap;
 import java.util.List;
 import org.eclipse.core.runtime.Assert;
 
+import reprotool.fm.nusmv.lang.nuSmvInputLanguage.AssignConstraint;
+import reprotool.fm.nusmv.lang.nuSmvInputLanguage.BooleanType;
+import reprotool.fm.nusmv.lang.nuSmvInputLanguage.CTLExpression;
+import reprotool.fm.nusmv.lang.nuSmvInputLanguage.CtlSpecification;
+import reprotool.fm.nusmv.lang.nuSmvInputLanguage.EnumType;
+import reprotool.fm.nusmv.lang.nuSmvInputLanguage.FairnessExpression;
+import reprotool.fm.nusmv.lang.nuSmvInputLanguage.InitConstraint;
+import reprotool.fm.nusmv.lang.nuSmvInputLanguage.MainModule;
+import reprotool.fm.nusmv.lang.nuSmvInputLanguage.Model;
+import reprotool.fm.nusmv.lang.nuSmvInputLanguage.NextBody;
+import reprotool.fm.nusmv.lang.nuSmvInputLanguage.NextExpression;
+import reprotool.fm.nusmv.lang.nuSmvInputLanguage.NuSmvInputLanguageFactory;
+import reprotool.fm.nusmv.lang.nuSmvInputLanguage.VarBody;
+import reprotool.fm.nusmv.lang.nuSmvInputLanguage.VariableDeclaration;
 import reprotool.model.usecase.UseCase;
 import reprotool.model.usecase.annotate.TemporalAnnotation;
 import reprotool.model.usecase.annotate.TemporalAnnotationGroup;
@@ -12,6 +26,7 @@ import reprotool.model.usecase.annotate.TemporalLogicFormula;
 
 public class NuSMVProject {
 	private List<NuSMVGenerator> generators;
+	private NuSmvInputLanguageFactory factory;
 	
 	/**
 	 * Global tracker of annotations in the whole project.
@@ -21,9 +36,84 @@ public class NuSMVProject {
 	private HashMap<UseCase, NuSMVGenerator> uc2gen = new HashMap<UseCase, NuSMVGenerator>();
 	private List<TemporalLogicFormula> formulas;
 	
+	public Model getModel() {
+		Model nusmvModel = factory.createModel();
+		MainModule mainModule = getMainModule(nusmvModel);
+		nusmvModel.getModules().add(mainModule);
+		for (NuSMVGenerator nusvm: generators) {
+			nusvm.fillAutomaton(uc2gen);
+			nusmvModel.getModules().add(nusvm.getModule());
+		}
+		return nusmvModel; 
+	}
+	
+	private MainModule getMainModule(Model model) {
+		MainModule mainModule = factory.createMainModule();
+		mainModule.setName("main");
+		addCTLFormulas(mainModule);
+		
+		StringBuffer varList = new StringBuffer();
+		
+		int c = 0;
+		for (NuSMVGenerator nusvm: generators) {
+			FairnessExpression fairness = factory.createFairnessExpression();
+			fairness.setFairnessExpr("p=p" + nusvm.getUseCaseId());
+			mainModule.getModuleElement().add(fairness);
+			if (c > 0) {
+				varList.append(",");
+			}
+			c++;
+			varList.append("p" + nusvm.getUseCaseId());
+		}
+		
+		VariableDeclaration pVar = factory.createVariableDeclaration();
+		pVar.setVar("VAR");
+		
+		VarBody pBody = factory.createVarBody();
+		pBody.setId("p");
+		
+		EnumType pType = factory.createEnumType();
+		pType.getVal().add("none");
+		for (NuSMVGenerator nusmv: generators) {
+			pType.getVal().add("p" + nusmv.getUseCaseId());
+		}
+		pBody.setType(pType);
+		
+		pVar.getVars().add(pBody);
+		mainModule.getModuleElement().add(pVar);
+		
+		InitConstraint initConstraint = factory.createInitConstraint();
+		initConstraint.setInitExpression("p in none");
+		mainModule.getModuleElement().add(initConstraint);
+		
+		AssignConstraint assignConstraint = factory.createAssignConstraint();
+		NextBody nextBody = factory.createNextBody();
+		nextBody.setVar("p");
+		
+		NextExpression nextExpression = factory.createNextExpression();
+		nextExpression.setSimpleExpression("case\n\t\tp=none : {" +
+			varList + "};\n\t\tTRUE : none;\n\tesac");
+		nextBody.setNext(nextExpression);
+		assignConstraint.getBodies().add(nextBody);
+		mainModule.getModuleElement().add(assignConstraint);
+		
+		addProcesses(mainModule, model);
+		addAnnotations(mainModule);
+		
+		return mainModule;
+	}
+	
+	private void addProcesses(MainModule module, Model model) {		
+		for (NuSMVGenerator nusmv: generators) {
+			nusmv.addProcess(module, model);
+		}
+	}
+	
 	public NuSMVProject (List<NuSMVGenerator> generators, List<TemporalLogicFormula> formulas) {
+		factory = NuSmvInputLanguageFactory.eINSTANCE;
 		this.generators = generators;
 		this.formulas = formulas;
+		
 		globalTracker = new HashMap<String, List<AnnotationEntry>>();
 		
 		processAnnotations();
@@ -53,152 +143,137 @@ public class NuSMVProject {
 		}
 	}
 	
-	public String getFormulas() {
-		StringBuffer buf = new StringBuffer();
+	private List<String> getAnnotatedVars(List<String> annotations) {
+		List<String> vars = new ArrayList<String>();
 		
+		for (String tag: globalTracker.keySet()) {
+			for (String annotName: annotations) {
+				if (tag.startsWith(annotName)) {
+					String var = tag.substring(annotName.length() + 1);
+					if (!vars.contains(var)) {
+						vars.add(var);
+						break;
+					}
+				}
+			}
+		}
+		
+		return vars;
+	}
+	
+	public void addCTLFormulas(MainModule module) {		
 		for (TemporalLogicFormula formula: formulas) {
 			Assert.isTrue(formula.eContainer() instanceof TemporalAnnotationGroup);
 			TemporalAnnotationGroup tGrp = (TemporalAnnotationGroup) formula.eContainer();
 			List<String> annotations = new ArrayList<String>();
 			for (TemporalAnnotation annot: tGrp.getMembers()) {
-//				System.out.println("Replacing " + annot.getName() + " in formula " + formula.getFormula());
 				annotations.add(annot.getName());
 			}
-//			annot.getName() si vlozim do pola annotations (bude mat napriklad {open, close})
-//			funkcia getAnnotatedVars(List<String> annotations) mi vrati prislusne premenne
-//			(Ak je v global trackeri open_x, close_y, tak vrati list {x,y})
-//			List<String> annotatedVars = getAnnotatedVars(annotations);
+
+			List<String> annotatedVars = getAnnotatedVars(annotations);
 			
-//			for (String var: annotatedVars) {
-//				String f = formula.getFormula();
-//				for (TemporalAnnotation annot: tGrp.getMembers()) {
-//					System.out.println("Replacing " + annot.getName() + " in formula " + formula.getFormula());
-//					f = f.replaceAll(annot.getName(), annot.getName + "_" + var);
-//				}
-//				System.out.println(f);
-//			}
-		}
-		
-		return buf.toString();
-	}
-	
-	public String getHeader() {
-		StringBuffer buf = new StringBuffer();
-		
-		buf.append("MODULE main\n\n");
-		buf.append("	-- FairnessConstraint\n");
-		
-		for (NuSMVGenerator nusmv: generators) {
-			buf.append("	FAIRNESS p=p" + nusmv.getUseCaseId() + ";\n");			
-		}
-		buf.append("\n");
-		
-		buf.append("	VAR p : {none,");
-		int c = 0;
-		for (NuSMVGenerator nusmv: generators) {
-			buf.append("p" + nusmv.getUseCaseId());
-			c++;
-			if (c < generators.size()) {
-				buf.append(",");
+			for (String var: annotatedVars) {
+				String f = formula.getFormula();
+				for (TemporalAnnotation annot: tGrp.getMembers()) {
+					if (f.contains(annot.getName())) {
+						f = f.replaceAll(annot.getName(), annot.getName() + "_" + var);
+					}
+				}
+				
+				System.out.println("Formula: " + f);
+				
+				CtlSpecification ctlSpec = factory.createCtlSpecification();
+				CTLExpression ctlExpr = factory.createCTLExpression();
+				ctlExpr.setSimpleExpression(f);
+				ctlSpec.setCtlExpression(ctlExpr);
+				module.getModuleElement().add(ctlSpec);
 			}
 		}
-		buf.append("};\n");
+	}
 		
-		buf.append("	INIT p in none;\n");
-		buf.append("	ASSIGN next(p) := case\n");
-		buf.append("		p=none : {");
+	private void addTraceAnnotation(String tag, MainModule module) {
+		VariableDeclaration tagVar = factory.createVariableDeclaration();
+		tagVar.setVar("VAR");	
+		VarBody tagBody = factory.createVarBody();
+		tagBody.setId(tag);
+		BooleanType tagType = factory.createBooleanType();
+		tagBody.setType(tagType);
+		tagVar.getVars().add(tagBody);
+		module.getModuleElement().add(tagVar);
+					
+		InitConstraint initConstraint = factory.createInitConstraint();
+		initConstraint.setInitExpression(tag + " in FALSE");
+		module.getModuleElement().add(initConstraint);
 		
-		c = 0;
-		for (NuSMVGenerator nusmv: generators) {
-			buf.append("p" + nusmv.getUseCaseId());
-			c++;
-			if (c < generators.size()) {
-				buf.append(",");
+		StringBuffer nextExpr = new StringBuffer();
+		nextExpr.append("case\n");		
+		
+		for (AnnotationEntry aEntry : globalTracker.get(tag)) {
+			for (String state: aEntry.states) {
+				nextExpr.append("\t\tx" + aEntry.automatonID + ".s=" + state + " : TRUE;\n");
 			}
 		}
-		buf.append("};\n");
+		nextExpr.append("\t\tTRUE : " + tag + ";\n\tesac");
 		
-		buf.append("		TRUE : none;\n");
-		buf.append("	esac;\n\n");
-		
-		return buf.toString();
+		AssignConstraint assignConstraint = factory.createAssignConstraint();
+		NextBody nextBody = factory.createNextBody();
+		nextBody.setVar(tag);
+		NextExpression nextExpression = factory.createNextExpression();
+		nextExpression.setSimpleExpression(nextExpr.toString());
+		nextBody.setNext(nextExpression);
+		assignConstraint.getBodies().add(nextBody);
+		module.getModuleElement().add(assignConstraint);
 	}
 	
-	public String getAutomata() {
-		StringBuffer buf = new StringBuffer();
-		
-		int c = 0;
-		for (NuSMVGenerator nusmv: generators) {
-			c++;
-			buf.append(nusmv.getAutomaton());
-			if (c < generators.size()) {
-				buf.append("\n\n");
-			}
-		}
-
-		return buf.toString();
-	}
-	
-	public String getProcesses() {
-		StringBuffer buf = new StringBuffer();
-		
-		for (NuSMVGenerator nusmv: generators) {
-			buf.append(nusmv.getProcess() + "\n\n");
-		}
-
-		return buf.toString();
-	}
-	
-	public String getTraceAnnotation(String tag) {
-		StringBuffer buf = new StringBuffer();
-		
-		buf.append("	VAR " + tag + " : boolean;\n");
-		buf.append("	INIT " + tag + " in FALSE;\n");
-		buf.append("	ASSIGN next(" + tag + ") := case\n");
-		
-		AnnotationEntry aEntry = globalTracker.get(tag).get(0);
-		for (String state: aEntry.states) {
-			buf.append("		x" + aEntry.automatonID + ".s=" + state + " : TRUE;\n");
-		}
-		buf.append("		TRUE : " + tag + ";\n");
-		buf.append("	esac;\n\n");
-
-		return buf.toString();
-	}
-	
-	public String getAnnotations() {		
-		StringBuffer buf = new StringBuffer();
-		
+	private void addAnnotations(MainModule module) {
 		for (String tag: globalTracker.keySet()) {
 			if (tag.matches("trace_.*")) {
-				buf.append(getTraceAnnotation(tag));
+				addTraceAnnotation(tag, module);
 				continue;
 			}
 			
 			if (tag.matches("on_.*")) {
 				continue;
 			}
+				
+			VariableDeclaration tagVar = factory.createVariableDeclaration();
+			tagVar.setVar("VAR");	
+			VarBody tagBody = factory.createVarBody();
+			tagBody.setId(tag);
+			BooleanType tagType = factory.createBooleanType();
+			tagBody.setType(tagType);
+			tagVar.getVars().add(tagBody);
+			module.getModuleElement().add(tagVar);
+						
+			InitConstraint initConstraint = factory.createInitConstraint();
+			initConstraint.setInitExpression(tag + " in FALSE");
+			module.getModuleElement().add(initConstraint);
 			
-			buf.append("	VAR " + tag + " : boolean;\n");
-			buf.append("	INIT " + tag + " in FALSE;\n");
-			buf.append("	ASSIGN next(" + tag + ") := FALSE\n");
+			StringBuffer nextExpr = new StringBuffer();
 			
+			nextExpr.append("FALSE\n");
 			for (AnnotationEntry aEntry: globalTracker.get(tag)) {
-				buf.append("		| x" + aEntry.automatonID + ".s in {" );
+				nextExpr.append("\t\t| x" + aEntry.automatonID + ".s in {" );
 				int c = 0;
 				for (String state: aEntry.states) {
 					if (c > 0) {
-						buf.append(",");
+						nextExpr.append(",");
 					}
 					c++;
-					buf.append(state);
+					nextExpr.append(state);
 				}
-				buf.append("}\n");
+				nextExpr.append("}\n");
 			}
-			buf.append("		;\n\n");
+			
+			AssignConstraint assignConstraint = factory.createAssignConstraint();
+			NextBody nextBody = factory.createNextBody();
+			nextBody.setVar(tag);
+			NextExpression nextExpression = factory.createNextExpression();
+			nextExpression.setSimpleExpression(nextExpr.toString());
+			nextBody.setNext(nextExpression);
+			assignConstraint.getBodies().add(nextBody);
+			module.getModuleElement().add(assignConstraint);
 		}
-		
-		return buf.toString();
 	}
 	
 	private void processAnnotations() {
--- trunk/modules/reprotool.fm.nusmv/src/reprotool/fm/nusmv/commands/SwprojToSMV.java
+++ trunk/modules/reprotool.fm.nusmv/src/reprotool/fm/nusmv/commands/SwprojToSMV.java
@@ -1,7 +1,5 @@
 package reprotool.fm.nusmv.commands;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -24,13 +22,8 @@ import org.eclipse.xtext.resource.SaveOptions;
 import org.eclipse.xtext.resource.SaveOptions.Builder;
 
 import reprotool.fm.nusmv.NuSMVGenerator;
-import reprotool.fm.nusmv.NuSMVGeneratorModel;
 import reprotool.fm.nusmv.NuSMVProject;
-import reprotool.fm.nusmv.NuSMVProjectModel;
 import reprotool.fm.nusmv.lang.NuSmvInputLanguageStandaloneSetup;
-import reprotool.fm.nusmv.lang.nuSmvInputLanguage.MainModule;
-import reprotool.fm.nusmv.lang.nuSmvInputLanguage.Model;
-import reprotool.fm.nusmv.lang.nuSmvInputLanguage.NuSmvInputLanguageFactory;
 import reprotool.model.swproj.SoftwareProject;
 import reprotool.model.usecase.UseCase;
 import reprotool.model.usecase.annotate.AnnotationGroup;
@@ -49,62 +42,6 @@ public class SwprojToSMV implements IHandler {
 	public void dispose() {
 		// TODO Auto-generated method stub
 	}
-		
-//	@Override
-//	public Object execute(ExecutionEvent event) throws ExecutionException {
-//		ISelection sel = HandlerUtil.getCurrentSelection(event);
-//		if(sel instanceof TreeSelection) {
-//			TreeSelection tsel = (TreeSelection) sel;
-//			IFile ifile = (IFile) tsel.getFirstElement();			
-//			final ResourceSet rs = new ResourceSetImpl();
-//			
-//			// Converting IFile to URI: see http://wiki.eclipse.org/index.php/EMF/FAQ#How_do_I_map_between_an_EMF_Resource_and_an_Eclipse_IFile.3F
-//			final URI uri = URI.createPlatformResourceURI(ifile.getFullPath().toString(), true);
-//			final Resource resource = rs.getResource(uri, true);
-//			
-//			EObject rootEObj = resource.getContents().get(0);
-//			
-//			if( ! (rootEObj instanceof SoftwareProject) ) {
-//				System.out.println("NOT a SWPROJ : " + rootEObj);
-//			}
-//
-//			SoftwareProject swproj = (SoftwareProject) rootEObj;
-//			System.out.println("FOUND SWPROJ : " + swproj);
-//			
-//			List<NuSMVGenerator> generators = new ArrayList<NuSMVGenerator>();
-//
-//			for (UseCase useCase : swproj.getUseCases()) {
-//				System.out.println("Found usecase " + useCase.getName());
-//				NuSMVGenerator nusmv = new NuSMVGenerator(useCase);
-//				generators.add(nusmv);
-//			}
-//			
-//			List<TemporalLogicFormula> formulas = new ArrayList<TemporalLogicFormula>();
-//			for (AnnotationGroup g: swproj.getAnnotationGroups()) {
-//				if (g instanceof TemporalAnnotationGroup) {
-//					TemporalAnnotationGroup t = (TemporalAnnotationGroup) g;
-//					formulas.addAll(t.getFormulas());
-//				}
-//			}
-//			
-//			NuSMVProject nuSMVProj = new NuSMVProject(generators, formulas);
-//						
-//			String fileName = CommonPlugin.resolve(uri).path() + ".nusmv";			
-//			try {
-//				BufferedWriter out = new BufferedWriter(new FileWriter(fileName));
-//				out.write(nuSMVProj.getFormulas());
-//				out.write(nuSMVProj.getHeader());
-//				out.write(nuSMVProj.getProcesses());
-//				out.write(nuSMVProj.getAnnotations());
-//				out.write(nuSMVProj.getAutomata());
-//				out.close();
-//			} catch (IOException e) {
-//				e.printStackTrace();
-//			}			
-//		}
-//		
-//		return null;
-//	}
 
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
@@ -133,18 +70,28 @@ public class SwprojToSMV implements IHandler {
 		SoftwareProject swproj = (SoftwareProject) rootEObj;
 		System.out.println("FOUND SWPROJ : " + swproj);
 		
-		List<NuSMVGeneratorModel> generators = new ArrayList<NuSMVGeneratorModel>();
+		List<NuSMVGenerator> generators = new ArrayList<NuSMVGenerator>();
 
 		for (UseCase useCase : swproj.getUseCases()) {
 			System.out.println("Found usecase " + useCase.getName());
-			NuSMVGeneratorModel nusmv = new NuSMVGeneratorModel(useCase);
+			NuSMVGenerator nusmv = new NuSMVGenerator(useCase);
 			generators.add(nusmv);
+		}
+		
+		List<TemporalLogicFormula> formulas = new ArrayList<TemporalLogicFormula>();
+		
+		for (AnnotationGroup aGrp: swproj.getAnnotationGroups()) {
+			if (!(aGrp instanceof TemporalAnnotationGroup)) {
+				continue;
+			}
+			TemporalAnnotationGroup tGrp = (TemporalAnnotationGroup) aGrp;
+			formulas.addAll(tGrp.getFormulas());
 		}
 		
 		URI outputUri = uri.appendFileExtension("nusmv");
 		System.out.println("Will be saved to : " + CommonPlugin.resolve(outputUri).path());
 		
-		NuSMVProjectModel nusmvProj = new NuSMVProjectModel(generators);
+		NuSMVProject nusmvProj = new NuSMVProject(generators, formulas);
 		
 		// serialization of the model
 		NuSmvInputLanguageStandaloneSetup.doSetup(); // activates the correct parser/serializer
@@ -164,7 +111,7 @@ public class SwprojToSMV implements IHandler {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+				
 		return null;
 	}
 	
