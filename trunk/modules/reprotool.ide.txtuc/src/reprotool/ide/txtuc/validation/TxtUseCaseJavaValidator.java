928a1932693f6bf4b0e3d128075f288bbf768979
rudotx <rudotx@7d00ebc3-3b19-6c51-bdff-128286bf8117>
2012-02-22 21:06:57 +0000

    added validation to the txtUseCase editor. You will need to run the mwe workflow in this plugin to compile the changes

--- trunk/modules/reprotool.ide.txtuc/src/reprotool/ide/txtuc/validation/TxtUseCaseJavaValidator.java
+++ trunk/modules/reprotool.ide.txtuc/src/reprotool/ide/txtuc/validation/TxtUseCaseJavaValidator.java
@@ -2,9 +2,14 @@ package reprotool.ide.txtuc.validation;
 
 import org.eclipse.xtext.validation.Check;
 
+import reprotool.ide.txtuc.txtUseCase.Condition;
+import reprotool.ide.txtuc.txtUseCase.ExtVarBlock;
+import reprotool.ide.txtuc.txtUseCase.ExtVarStep;
+import reprotool.ide.txtuc.txtUseCase.ExtVarUnit;
 import reprotool.ide.txtuc.txtUseCase.MainScenarioBlock;
 import reprotool.ide.txtuc.txtUseCase.MainScenarioStep;
 import reprotool.ide.txtuc.txtUseCase.TxtUseCasePackage;
+import reprotool.ide.txtuc.txtUseCase.UseCase;
  
 
 public class TxtUseCaseJavaValidator extends AbstractTxtUseCaseJavaValidator {
@@ -25,6 +30,137 @@ public class TxtUseCaseJavaValidator extends AbstractTxtUseCaseJavaValidator {
 		}
 	}
 	
+	@Check
+	public void checkExtVarLabelSequence(ExtVarStep step) {
+		String stepLabel = step.getLabel();
+		ExtVarUnit u = (ExtVarUnit) step.eContainer();
+		if (u.getCondition() == null) {
+			return;
+		}
+				
+		String condLabel = u.getCondition().getLabel();
+		
+		if (!(stepLabel.startsWith(condLabel))) {
+			String message = "Label of this step needs to start with the sequence \"" + condLabel + "\"";
+			error(message, TxtUseCasePackage.Literals.EXT_VAR_STEP__LABEL);
+			return;
+		}
+		
+		int index = u.getSteps().indexOf(step);
+		String ext = Integer.toString(index + 1);
+		String expectedLabel = condLabel + ext;
+		
+		if (!stepLabel.equals(expectedLabel)) {
+			String message = String.format("Expected step label \"%s\" found \"%s\"", expectedLabel, stepLabel);
+			error(message, TxtUseCasePackage.Literals.EXT_VAR_STEP__LABEL);			
+		}
+	}
+	
+	@Check
+	public void checkConditionLabel(Condition cond) {
+		String condLabel = cond.getLabel();
+		String stepLabel = condLabel.substring(0, condLabel.length() - 1);
+		ExtVarUnit u = (ExtVarUnit) cond.eContainer();
+		ExtVarBlock extVarBlock = (ExtVarBlock) u.eContainer();
+		UseCase uc = (UseCase) extVarBlock.eContainer();
+		
+		int counter = 0;
+		for (ExtVarUnit unit: uc.getExtensionsBlock().getUnits()) {
+			Condition c = unit.getCondition();
+			if (c.getLabel().equals(condLabel)) {
+				counter++;
+				if (counter > 1) {
+					break;
+				}
+			}
+		}
+		if (counter <= 1) {
+			for (ExtVarUnit unit: uc.getVariationsBlock().getUnits()) {
+				Condition c = unit.getCondition();
+				if (c.getLabel().equals(condLabel)) {
+					counter++;
+				}
+			}
+		}
+		
+		if (counter > 1) {
+			String message = "The condition label " + condLabel + " has been used multiple times.";
+			error(message, TxtUseCasePackage.Literals.CONDITION__LABEL);
+			return;
+		}
+		
+		Character lastChar = condLabel.charAt(condLabel.length() - 1);
+		boolean found = false;
+		
+		if (lastChar == 'a') {
+			found = true;
+		}
+		
+		if ((!found) && (Character.isLetterOrDigit(lastChar))) {
+			int value = (int) lastChar;
+			Character prev = (char) (value - 1);
+			String prevCondLabel = stepLabel + prev;
+			for (ExtVarUnit unit: extVarBlock.getUnits()) {
+				Condition c = unit.getCondition();
+				if (prevCondLabel.equals(c.getLabel())) {
+					found = true;
+					break;
+				}
+			}
+			
+			if ((!found) && (extVarBlock == uc.getVariationsBlock())) {
+				for (ExtVarUnit unit: uc.getExtensionsBlock().getUnits()) {
+					Condition c = unit.getCondition();
+					if (prevCondLabel.equals(c.getLabel())) {
+						found = true;
+						break;
+					}
+				}
+			}
+			
+			if (!found) {
+				String message = "The condition with label " + prevCondLabel + " is missing.";
+				error(message, TxtUseCasePackage.Literals.CONDITION__LABEL);
+				return;
+			}
+		}
+		
+		found = false;
+		for (MainScenarioStep step: uc.getMainScenarioBlock().getSteps()) {
+			if (step.getLabel().equals(stepLabel)) {
+				found = true;
+				break;
+			}
+		}
+		
+		if (!found) {
+			outer: for (ExtVarUnit unit: uc.getExtensionsBlock().getUnits()) {
+				for (ExtVarStep step: unit.getSteps()) {
+					if (step.getLabel().equals(stepLabel)) {
+						found = true;
+						break outer;
+					}
+				}
+			}
+		}
+		
+		if (!found) {
+			outer: for (ExtVarUnit unit: uc.getVariationsBlock().getUnits()) {
+				for (ExtVarStep step: unit.getSteps()) {
+					if (step.getLabel().equals(stepLabel)) {
+						found = true;
+						break outer;
+					}
+				}
+			}
+		}
+		
+		if (!found) {
+			String message = "The use-case does not contain a step with label " + stepLabel;
+			error(message, TxtUseCasePackage.Literals.CONDITION__LABEL);
+			return;
+		}
+	}
 	
 	// TODO - ExtVarBlock - step labels form sequence (i.e. - 1a1, 1a3 <- missing 1a2)
 	// TODO - ExtVarBlock - steps belong to condition (i.e. - 1a1, 1a2 after 2b <- steps should start with 2b)
