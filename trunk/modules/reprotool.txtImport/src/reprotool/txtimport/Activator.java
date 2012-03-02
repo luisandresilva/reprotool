8de46bbca6ee85850369a1636f47ea912da2c5b9
rudotx@gmail.com <rudotx@gmail.com@7d00ebc3-3b19-6c51-bdff-128286bf8117>
2012-03-02 17:21:45 +0000

    added citymap project to the reprotool example projects

--- trunk/modules/reprotool.txtImport/src/reprotool/txtimport/Activator.java
+++ trunk/modules/reprotool.txtImport/src/reprotool/txtimport/Activator.java
@@ -1,6 +1,10 @@
 package reprotool.txtimport;
 
 import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.ui.console.ConsolePlugin;
+import org.eclipse.ui.console.IConsole;
+import org.eclipse.ui.console.IConsoleManager;
+import org.eclipse.ui.console.MessageConsole;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
 
@@ -46,6 +50,27 @@ public class Activator extends AbstractUIPlugin {
 	 */
 	public static Activator getDefault() {
 		return plugin;
+	}
+	
+	/**
+	 * Creating a new console within the running Eclipse instance.
+	 * @param name
+	 * @return 
+	 */
+	public MessageConsole findConsole() {
+		ConsolePlugin plugin = ConsolePlugin.getDefault();
+		IConsoleManager consoleManager = plugin.getConsoleManager();
+
+		for (IConsole console : consoleManager.getConsoles()) {
+			if (PLUGIN_ID.equals(console.getName())) {
+				return (MessageConsole) console;
+			}
+		}
+
+		MessageConsole console = new MessageConsole(PLUGIN_ID, null);
+		consoleManager.addConsoles(new IConsole[] { console });
+
+		return console;
 	}
 
 	/**
