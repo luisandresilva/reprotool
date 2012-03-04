128ece04aadc95d5cb59cff386f1f6e44595359a
jiri.vinarek@gmail.com <jiri.vinarek@gmail.com@7d00ebc3-3b19-6c51-bdff-128286bf8117>
2012-03-04 09:48:36 +0000

    minor cleanup

--- trunk/modules/reprotool.ide/src/reprotool/ide/utils/Utils.java
+++ trunk/modules/reprotool.ide/src/reprotool/ide/utils/Utils.java
@@ -1,9 +1,5 @@
 package reprotool.ide.utils;
 
-import org.eclipse.emf.ecore.EObject;
-import org.eclipse.emf.ecore.resource.ResourceSet;
-import org.eclipse.emf.edit.domain.EditingDomain;
-import org.eclipse.emf.edit.domain.IEditingDomainProvider;
 import org.eclipse.jface.viewers.ISelectionProvider;
 import org.eclipse.swt.events.FocusAdapter;
 import org.eclipse.swt.events.FocusEvent;
@@ -14,20 +10,6 @@ import org.eclipse.swt.widgets.Control;
  *
  */
 public class Utils {
-	
-	/**
-	 * Retrieves {@link EditingDomain} for given {@link EObject}
-	 * 
-	 * @param eobject
-	 * @return
-	 */
-//	public static EditingDomain getEditingDomain(EObject eobject) {
-//		ResourceSet resourceSet = eobject.eResource().getResourceSet();
-//		// TODO - jvinarek - use guava validation ?
-//		assert(resourceSet instanceof IEditingDomainProvider);
-//		EditingDomain editingDomain = ((IEditingDomainProvider)resourceSet).getEditingDomain(); 
-//        return editingDomain; 
-//	}
 	
 	/**
 	 * Adds focus listener to control changing selection provider to the given
