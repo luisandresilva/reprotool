1360425716946767a1f4c89464a09507c6d90797
jiri.vinarek@gmail.com <jiri.vinarek@gmail.com@7d00ebc3-3b19-6c51-bdff-128286bf8117>
2012-03-11 16:09:17 +0000

    changed txtuc wizard to automatically open created file

--- trunk/modules/reprotool.ide/src/reprotool/ide/txtuc/ui/wizard/NewTxtucWizard.java
+++ trunk/modules/reprotool.ide/src/reprotool/ide/txtuc/ui/wizard/NewTxtucWizard.java
@@ -3,8 +3,14 @@ package reprotool.ide.txtuc.ui.wizard;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.wizard.Wizard;
+import org.eclipse.ui.IEditorDescriptor;
 import org.eclipse.ui.INewWizard;
 import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.part.FileEditorInput;
 
 public class NewTxtucWizard extends Wizard implements INewWizard {
 
@@ -24,12 +30,34 @@ public class NewTxtucWizard extends Wizard implements INewWizard {
 	@Override
 	public boolean performFinish() {
 		IFile file = newFileWizardPage.createNewFile();
+
+		openEditor(file);
+
 		return file != null;
+	}
+
+	private void openEditor(IFile file) {
+		FileEditorInput editorInput = new FileEditorInput(file);
+
+		IWorkbench wb = PlatformUI.getWorkbench();
+		IWorkbenchWindow win = wb.getActiveWorkbenchWindow();
+		IWorkbenchPage page = win.getActivePage();
+
+		IEditorDescriptor desc = PlatformUI.getWorkbench().getEditorRegistry()
+				.findEditor("reprotool.ide.txtuc.TxtUseCase");
+
+		try {
+			page.openEditor(editorInput, desc.getId());
+		} catch (PartInitException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	}
 
 	@Override
 	public void init(IWorkbench workbench, IStructuredSelection selection) {
 		this.selection = selection;
 	}
-	
+
 }
