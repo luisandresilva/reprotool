32d1a55f0512ccdbb3aeb986833f4b412944d85c
jiri.vinarek@gmail.com <jiri.vinarek@gmail.com@7d00ebc3-3b19-6c51-bdff-128286bf8117>
2012-03-12 18:36:29 +0000

    fix of the use case step delete

--- trunk/modules/reprotool.model/src/reprotool/model/usecase/impl/UseCaseImplCustom.java
+++ trunk/modules/reprotool.model/src/reprotool/model/usecase/impl/UseCaseImplCustom.java
@@ -1,21 +1,27 @@
 package reprotool.model.usecase.impl;
 
-import org.eclipse.emf.common.util.BasicEList;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.ecore.util.EcoreEList;
 
 import reprotool.model.usecase.Scenario;
 import reprotool.model.usecase.UseCaseStep;
+import reprotool.model.usecase.UsecasePackage;
 
 public class UseCaseImplCustom extends UseCaseImpl {
 
 	@Override
 	public EList<UseCaseStep> getAllUseCaseStepsShortcut() {
-		EList<UseCaseStep> result = new BasicEList<UseCaseStep>();
-		getStepsFromScenario(this.getMainScenario(), result);
-		return result;
+		ArrayList<UseCaseStep> steps = new ArrayList<UseCaseStep>();
+		getStepsFromScenario(this.getMainScenario(), steps);
+		return new EcoreEList.UnmodifiableEList<UseCaseStep>(this,
+                UsecasePackage.eINSTANCE.getUseCase_AllUseCaseStepsShortcut(),
+                steps.size(), steps.toArray());
 	}
 	
-	private void getStepsFromScenario(Scenario scenario, EList<UseCaseStep> result) {
+	private void getStepsFromScenario(Scenario scenario, List<UseCaseStep> result) {
 		// check is needed, exception was thrown when deleting actor
 		if (scenario == null) {
 			return;
