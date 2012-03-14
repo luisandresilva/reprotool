5041cd4a74817943ca3e51715a1c451bb16d7833
viliam.simko@gmail.com <viliam.simko@gmail.com@7d00ebc3-3b19-6c51-bdff-128286bf8117>
2012-03-14 14:19:31 +0000

    - added acronyms in latex - removed tutorial chapter - improved script that converts SVG/ODG files to PDF - removed transparency from XtextWorkflow

--- trunk/documentation/latex/images/convert2pdf.sh
+++ trunk/documentation/latex/images/convert2pdf.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # created by Viliam Simko 2007-01-12
+# 2012-02-29 : Viliam Simko : added "crop" switch, which implies cropping PDF files without converting from source format
 # 2011-12-23 : Viliam Simko : fixed error message if *.svg or *.odg files do not exist
 # 2011-12-19 : Viliam Simko : added SVG->PDF using Inkscape
 # 2011-05-05 : Viliam Simko : added system_check
@@ -22,22 +23,26 @@ function system_check()
 		echo "Try to install xterm"
 	}
 
-	cmdavail pdfcrop || {
-		echo "The pdfcrop utility is not installed"
-		echo "This should be a small python script"
-		echo "On Ubuntu try to install the texlive-extra-utils package"
-		exit 1
-	}
 
 	cmdavail unoconv || {
 		echo "The unoconv utility is not installed"
-		echo "Try to install the unoconv package and libre/open office"
-		exit 1
+		echo "Try to install the unoconv package and libreoffice/openoffice"
+	}
+
+	{ unoconv; } &> /dev/null
+	[ "$?" -eq 0 ] || {
+		echo "WARNING: LibreOffice/OpenOffice UNO bridge (unoconv) does not work properly!"
 	}
 
 	cmdavail inkscape || {
 		echo "Inkscape is not installed. It is required for SVG->PDF conversion."
 		echo "Try to install inkscape, e.g. sudo apt-get install inkscape"
+	}
+
+	cmdavail pdfcrop || {
+		echo "The pdfcrop utility is not installed"
+		echo "This should be a small python script."
+		echo "On Ubuntu try to install the texlive-extra-utils package"
 		exit 1
 	}
 }
@@ -96,18 +101,33 @@ do
 	SUFFIX="${INPUTNAME##*.}"
 	PDFNAME="$BASENAME".pdf
 
-	if [ "$INPUTNAME" -nt "$PDFNAME" ]
+	if [ "$INPUTNAME" -nt "$PDFNAME" -o "$INPUTNAME" -ot "$PDFNAME" ] # modification time differs
 	then
+
+		if [ "$1" = "crop" ]
+		then
+
+			if [ ! -f "$PDFNAME" ]
+			then
+				echo "PDF file not found: $PDFNAME"
+				continue
+			fi
+
+			crop_pdf_file "$PDFNAME"
+			# The original file and the created PDF files will have the same modification time
+			synchronize_mtime "$INPUTNAME" "$PDFNAME"
+			continue
+		fi
 
 		# run the conversion
 		echo -n "  Converting to PDF: $INPUTNAME ... "
 		convert_"$SUFFIX"_to_pdf "$INPUTNAME" "$PDFNAME"
-
 		# The original file and the created PDF files will have the same modification time
 		synchronize_mtime "$INPUTNAME" "$PDFNAME"
-	else
-		echo "  No conversion needed for: $INPUTNAME."
+		continue
 	fi
+
+	echo "  No conversion needed for: $INPUTNAME."
 done
 
 # ---------------------------- 
